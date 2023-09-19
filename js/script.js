f59531bf47f950ef6d2d53973b12a169e6133864
MonoDG <monodg@proton.me>
2023-09-19 17:02:12 -0400

    Add function getComputerChoice
    This function returns a random choice between [Rock, Paper, Scissors].

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// Return the computer choice whether it's Rock, Paper or Scissors
+function getComputerChoice() {
+    let randomNum = Math.floor((Math.random() * 3)) + 1;
+    if (randomNum === 1) {
+        return "Rock";
+    } else if (randomNum === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
